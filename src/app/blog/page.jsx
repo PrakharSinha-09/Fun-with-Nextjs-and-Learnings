9a4f93a4cf07ecc5b9262c9f64f95814710fb383
Prakhar Sinha <prakhar.sinha2k2@gmail.com>
2025-01-18 12:45:15 +0530

    some fixes and added some static data for portfolio section

--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -1,9 +1,52 @@
-import React from 'react'
-import styles from './page.module.css'
-const Blog = () => {
-  return (
-    <div className={styles.container}>Blog</div>
-  )
+import styles from "./page.module.css";
+import Link from "next/link";
+import Image from "next/image";
+import { notFound } from "next/navigation";
+
+async function getData() {
+  const res = await fetch("http://localhost:3000/api/posts", {
+    cache: "no-store",
+  });
+
+  if (!res.ok) {
+     if (!res.ok) {
+            return notFound()          //this notfound page, nextjs provides us out of the box via next/navigation
+    }
+  }
+
+  return res.json();
 }
+
+export async function generateMetadata() {
+    return {
+        title: "Blog",
+        description: "This is a blog page",
+    };
+}
+
+const Blog = async () => {
+  const data = await getData();
+  return (
+    <div className={styles.mainContainer}>
+      {data.map((item) => (
+        <Link href={`/blog/${item._id}`} className={styles.container} key={item.id}>
+          <div className={styles.imageContainer}>
+            <Image
+              src={item.img}
+              alt=""
+              width={400}
+              height={250}
+              className={styles.image}
+            />
+          </div>
+          <div className={styles.content}>
+            <h1 className={styles.title}>{item.title}</h1>
+            <p className={styles.desc}>{item.desc}</p>
+          </div>
+        </Link>
+      ))}
+    </div>
+  );
+};
 
 export default Blog;
